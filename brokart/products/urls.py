12315fdac6a480234177c7da1eec229172c77f9b
Niyad <niyadsystem@gmail.com>
2024-06-13 22:25:30 +0530

    desinged the layout & added home and product page

--- brokart/products/urls.py
+++ brokart/products/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index),
+    path('',views.index,name='home'),
+    path('product_list',views.list_products,name='list_product')
+
 ]
 
