a8c062dbc0358a5404102d424c5c13311b040b3f
Niyad <niyadsystem@gmail.com>
2024-06-25 19:31:07 +0530

    priority added in product page, featured_products, latest_product

--- brokart/products/views.py
+++ brokart/products/views.py
@@ -4,7 +4,14 @@ from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    featured_product=Product.objects.order_by('priority')[:4]
+    latest_product=Product.objects.order_by('-id')[:4]
+    context={
+        'featured_product':featured_product,
+        'latest_product':latest_product
+    }
+    print(context)
+    return render(request,'index.html',context)
 
 def list_products(request):
     """_summary_
@@ -18,11 +25,13 @@ def list_products(request):
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
-    product_paginator=Paginator(product_list,4)
+    product_list=Product.objects.order_by('-priority')
+    product_paginator=Paginator(product_list,2)
     product_list=product_paginator.get_page(page)
     context={'products':product_list}
     return render(request,'products.html',context)
 
-def detail_product(request):
-    return render(request,'product_detail.html')
+def detail_product(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request,'product_detail.html',context)
